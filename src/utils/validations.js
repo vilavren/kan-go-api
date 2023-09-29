753ff0859f3657afdc4836dbcb5718f31934177f
vitalii <vilavren@gmail.com>
2023-09-29 16:40:50 +0500

    fix: user login and registration

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -8,17 +8,9 @@ export const loginValidation = [
 ]
 
 export const registerValidation = [
+  body('name', 'Укажите имя').isLength({ min: 3 }),
   body('email', 'Неверный формат почты').isEmail(),
   body('password', 'Пароль должен содержать минимум 5 символов').isLength({
     min: 5,
   }),
-  body('fullName', 'Укажите имя').isLength({ min: 3 }),
-  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
-]
-
-export const postCreateValidation = [
-  body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
-  body('text', ' Введите текст статьи').isLength({ min: 3 }).isString(),
-  body('tags', 'Неверный формат тегов').optional().isString(),
-  body('imageUrl', 'Неверная ссылка на аватарку').optional().isString(),
 ]
